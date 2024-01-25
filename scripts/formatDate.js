8c43b8835078236864a48b539eaf503af01410f9
AlionaKolb <alvibars@mail.ru>
2024-01-25 17:20:53 +0300

    Неделя 2

--- scripts/formatDate.js
+++ scripts/formatDate.js
@@ -1,15 +1,22 @@
-const monts = ['января', 'февраля', 'марта', 'апреля', 'мая', 'июня', 'июля', 'августа', 'сентября', 'октября', 'ноября', 'декабря'];
+"use strict";
+const MONTHS = ['января', 'февраля', 'марта', 'апреля', 'мая', 'июня', 'июля', 'августа', 'сентября', 'октября', 'ноября', 'декабря'];
 
 export function formatDate(date) {
 
-    let dd = date.getDate();
-    if (dd < 10) dd = '0' + dd;
+    let newDate = date.getDate();
+    if (newDate < 10) {
+        newDate = '0' + newDate
+    };
 
-    let mm = monts[date.getMonth()];
-    if (mm < 10) mm = '0' + mm;
+    let newMonts = MONTHS[date.getMonth()];
+    if (newMonts < 10) {
+        newMonts = '0' + newMonts
+    };
 
-    let yy = date.getFullYear();
-    if (yy < 10) yy = '0' + yy;
+    let newYear = date.getFullYear();
+    if (newYear < 10) {
+        newYear = '0' + newYear
+    };
 
-    return dd + ' ' + mm + ' ' + yy;
+    return `${newDate} ${newMonts} ${newYear}`;
 }
--- scripts/postPublish.js
+++ scripts/postPublish.js
@@ -1,52 +1,44 @@
-import { closeAndClearEntry, showModal, addContent } from "./modalEntry.js";
-import { countPosts, photoCount } from "./countPosts.js";
+"use strict";
+import { closeAndClearEntry, showModal, addContent, addOverlay } from "./modalEntry.js";
 import { showSuccessMessage, showFailMessage } from "./showMessage.js";
 import { makePostByTemplate } from "./useTemplate.js"
 import { formatDate } from "./formatDate.js";
+import { formdata, postText } from "./updatePreview.js";
 
-export const fileInput = document.querySelector("#file-upload");
 export const previewPostModal = document.querySelector(".preview-post-modal");
 export const textCounter = document.querySelector(".text-counter");
-export const postText = document.querySelector("#post-text");
-export const postHashtags = document.querySelector("#post-hashtags");
+export const photoCount = document.querySelector("#photo-count");
 export const photosContent = document.querySelector(".photos__content");
+const postPhoto = document.querySelector("#post-photo");
+const shareHashtads = document.querySelector(".post-hashtags a");
+const infoTime = document.querySelector(".account-info__time");
+const statisticsLikes = document.querySelector(".statistics__likes span");
+const statisticsComments = document.querySelector(".statistics__comments span");
+
 const btn = document.querySelector(".photos__button button");
 const like = document.querySelector(".statistics__likes");
 const commentsBtn = document.querySelector(".comments-button");
+const delPost = document.querySelector("#delete-post");
+const postComment = document.querySelector("#post-comment");
 
-fileInput.setAttribute('accept', 'image/*');
-fileInput.setAttribute('name', 'file');
-
-
-const urlPost = "https://c-gallery.polinashneider.space/api/v1/posts/";
-const urlGet = "https://c-gallery.polinashneider.space/api/v1/users/me/posts/";
-const myToken = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzA3MzM0MTI4LCJpYXQiOjE3MDI0OTU3MjgsImp0aSI6IjZkOWEwODNhOTk4NDQwY2Q5YzQ2ODE5N2IwODkzNWRjIiwidXNlcl9pZCI6NDN9.cf9FEPwqqPztSDdMD3RFYUtBLzpfI3jRg2zkInQWCcI";
-
-let formdata = new FormData();
-
-export function sendPost(event) {
-    event.preventDefault(); // кроме drag and drop
-    formdata.append("image", fileInput.files[0]); // изменить для drag and drop
-    formdata.append("tags", postHashtags.value);
-    formdata.append("text", postText.value);
-
-    postRequest();
-}
+const URLPOST = "https://c-gallery.polinashneider.space/api/v1/posts/";
+const URLGET = "https://c-gallery.polinashneider.space/api/v1/users/me/posts/";
+const MYTOKEN = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzA3MzM0MTI4LCJpYXQiOjE3MDI0OTU3MjgsImp0aSI6IjZkOWEwODNhOTk4NDQwY2Q5YzQ2ODE5N2IwODkzNWRjIiwidXNlcl9pZCI6NDN9.cf9FEPwqqPztSDdMD3RFYUtBLzpfI3jRg2zkInQWCcI";
 
 export function postRequest() {
 
-    fetch(urlPost, {
+    fetch(URLPOST, {
 
             method: 'POST',
             headers: {
-                Authorization: myToken,
+                Authorization: MYTOKEN,
             },
             body: formdata
         })
         .then((result) => {
             if (result.ok) {
                 showSuccessMessage();
-                countPosts();
+                photoCount.textContent = ++photoCount.textContent;
                 addContent();
                 return result.json();
             } else {
@@ -67,7 +59,7 @@ export function postRequest() {
 getResponse();
 
 let posts = [];
-let postsPortion = 9;
+const POSTSPORTION = 9;
 let slicedData = [];
 let numerOfShownItems = 9;
 
@@ -76,7 +68,7 @@ btn.addEventListener('click', () => {
     slicedData = posts.slice(0, numerOfShownItems);
     console.log('got here', slicedData);
     const fragment = new DocumentFragment();
-    photosContent.innerHTML = '';
+    photosContent.textContent = '';
     for (let item of slicedData) {
         fragment.append(makePostByTemplate(item.image, item.id));
     };
@@ -87,26 +79,26 @@ btn.addEventListener('click', () => {
 });
 
 export function getResponse() {
-    fetch(urlGet, {
+    fetch(URLGET, {
             method: 'GET',
             headers: {
-                Authorization: myToken,
+                Authorization: MYTOKEN,
             }
         })
         .then((response) => {
             return response.json();
         })
         .then((data) => {
-            photosContent.innerHTML = '';
+            photosContent.textContent = '';
             posts = data;
 
-            photoCount.innerHTML = data.length;
+            photoCount.textContent = data.length;
             addContent();
 
             const postsLength = data.length;
-            const slicedData = data.slice(0, postsPortion);
+            const slicedData = data.slice(0, POSTSPORTION);
 
-            if (postsLength > postsPortion) {
+            if (postsLength > POSTSPORTION) {
                 btn.classList.remove('hidden');
             } else {
                 btn.classList.add('hidden')
@@ -123,6 +115,8 @@ export function getResponse() {
         })
 }
 
+
+
 let currentId;
 let currentPostInfo;
 
@@ -134,17 +128,16 @@ function openPreview(event) {
         currentId = +postElement.id;
         currentPostInfo = posts.find((item) => item.id === currentId);
 
-        let date = currentPostInfo.created_at;
+        const date = currentPostInfo.created_at;
         const dateOfCreation = formatDate(new Date(date));
-        console.log(currentPostInfo.id);
         previewPostModal.classList.add("active");
-        showModal();
-        document.querySelector("#post-photo").src = currentPostInfo.image;
-        document.querySelector(".post-text").innerHTML = currentPostInfo.text;
-        document.querySelector(".post-hashtags a").innerHTML = currentPostInfo.tags.join(' ');
-        document.querySelector(".account-info__time").innerHTML = dateOfCreation;
-        document.querySelector(".statistics__likes span").innerHTML = currentPostInfo.likes;
-        document.querySelector(".statistics__comments span").innerHTML = currentPostInfo.comments.length;
+        addOverlay();
+        postPhoto.src = currentPostInfo.image;
+        postText.textContent = currentPostInfo.text;
+        shareHashtads.textContent = currentPostInfo.tags.join(' ');
+        infoTime.textContent = dateOfCreation;
+        statisticsLikes.textContent = currentPostInfo.likes;
+        statisticsComments.textContent = currentPostInfo.comments.length;
 
     }
 
